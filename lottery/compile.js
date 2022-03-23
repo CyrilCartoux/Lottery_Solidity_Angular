6134443edb214aaefe9a4307648b38042636ee95
CITYZEN\cyril.cartoux <cyril.cartoux@arche-mc2.fr>
2022-03-23 11:48:00 +0100

    quand on compile , on veux créer un fichier qui contiendra le bytecode et l'interface

--- lottery/compile.js
+++ lottery/compile.js
@@ -5,4 +5,9 @@ const solc = require("solc");
 const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
 const source = fs.readFileSync(lotteryPath, "utf8");
 
-module.exports = solc.compile(source, 1).contracts[":Lottery"];
+const {interface, bytecode} = solc.compile(source, 1).contracts[":Lottery"];
+// store the ABI + bytecode to a compiledContract.json file
+const artifact = JSON.stringify({interface,bytecode}, null, 2);
+fs.writeFileSync("compiledContract.json", artifact);
+
+module.exports = {interface,bytecode}
