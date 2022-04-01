e7b4f2ca7f344b9468a6f89b6af6c84cd5311f9e
CITYZEN\cyril.cartoux <cyril.cartoux@arche-mc2.fr>
2022-04-01 16:37:53 +0200

    Campaign factory to create new instance of a campaign

---
+++ lottery/contracts/CampaignFactory.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+import './Campaign.sol';
+ 
+pragma solidity ^0.8.9;
+
+// Contract factory to create Campaigns 
+contract CampaignFactory {
+    
+    address public manager;
+    address[] public deployedCampaigns;
+    uint campaignId;
+    mapping(uint=>Campaign) public campaigns;
+    mapping(uint=>address) public campaignsOwners;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    // Creates a new campaign
+    function createCampaign(string memory lib, uint minimum) public {
+        Campaign newCampaign = new Campaign(lib, minimum);
+        campaigns[campaignId] = newCampaign;
+        campaignsOwners[campaignId] = msg.sender;
+        campaignId++;
+        deployedCampaigns.push(address(newCampaign));
+    }
+
+    function getDeployedCampaigns() public view returns (address[] memory) {
+        return deployedCampaigns;
+    }
+    
+}
